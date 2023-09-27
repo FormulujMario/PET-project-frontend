b2fd3daecb720782102919c6872adf2170432614
FormulujMario <msh89.mail@gmail.com>
2023-09-27 17:36:13 +0200

    add title section

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const ulMenu = document.querySelector(".menu-list ul");
 menuList.forEach((item) => {
   ulMenu.insertAdjacentHTML("beforeend", `<li>${item}</li>`);
 });
+
+let title = document.querySelector("main h1").innerHTML;
+if (title.length > 12) {
+  document.querySelector("main h1").innerHTML = "";
+  title.split(" ").forEach((word) => {
+    document
+      .querySelector("main h1")
+      .insertAdjacentHTML("beforeend", `<span>${word}</span>`);
+  });
+}
