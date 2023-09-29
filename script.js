ff742444766817867d3466263c109de4723997c9
FormulujMario <msh89.mail@gmail.com>
2023-09-29 15:42:02 +0200

    add slider on top

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ if (title.length > 12) {
       .insertAdjacentHTML("beforeend", `<span>${word}</span>`);
   });
 }
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
