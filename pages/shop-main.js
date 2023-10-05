ba4536d302337ecb3848527ae66da14c889359de
FormulujMario <msh89.mail@gmail.com>
2023-10-05 11:42:24 +0200

    add shop-main page template

---
+++ pages/shop-main.js
@@ -0,0 +1,16 @@
+import { menuListDisplay } from "../modules/header/header.js";
+import { pageTitleDisplay } from "../modules/page-title.js";
+import {
+  backToTopButton,
+  footerMenuListDisplay,
+  socNetIconsDisplay,
+} from "../modules/footer/footer.js";
+// ======================= Menu header =======================
+menuListDisplay();
+// ======================= Page title =======================
+pageTitleDisplay();
+// ======================= Back to top button =======================
+backToTopButton();
+// ======================= Footer menu list =========================
+footerMenuListDisplay();
+socNetIconsDisplay();
