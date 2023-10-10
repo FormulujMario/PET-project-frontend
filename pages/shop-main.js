786105f58487bff957809f677fc11427e09c2d2d
FormulujMario <msh89.mail@gmail.com>
2023-10-10 19:23:35 +0200

    fix code for future work

--- pages/shop-main.js
+++ pages/shop-main.js
@@ -32,7 +32,7 @@ const categoryDisplay = (section, list) => {
             <div class="shop-item">
             <div class="shop-item-img"><img src="${element.img}" alt=""></div>
             <div class="shop-item-title"><h4>${element.name}</h4><h4>${element.price} ${element.currency}</h4></div>
-            <p>${element.description}</p>
+            <p>${element.style} / abv ${element.alcohol}% / ibu ${element.color} / ${element.volume}l</p>
             </div>`
     );
   });
@@ -181,6 +181,7 @@ SORT_BY.forEach((element) => {
               </div>`
   );
 });
+// ======================= sort by function (ex high to low price) =======================
 const onclickSortBy = (div, property)=>{
     div.addEventListener("click", () => {
     SHOP_MAIN_BEERS_LIST.sort(function (a, b) {
@@ -189,46 +190,18 @@ const onclickSortBy = (div, property)=>{
     categoryDisplay(shopBeersSection, SHOP_MAIN_BEERS_LIST);
   });
 }
-
 // ======================= popularity =======================
 const popularityDiv = document.querySelector(".popularity");
 onclickSortBy(popularityDiv, 'popularity');
-
-// popularityDiv.addEventListener("click", () => {
-//   SHOP_MAIN_BEERS_LIST.sort(function (a, b) {
-//     return a.popularity - b.popularity;
-//   });
-//   categoryDisplay(shopBeersSection, SHOP_MAIN_BEERS_LIST);
-// });
 // ======================= average rating =======================
 const averageRatingDiv = document.querySelector(".average-rating");
 onclickSortBy(averageRatingDiv, 'averageRating');
-
-// averageRatingDiv.addEventListener("click", () => {
-//   SHOP_MAIN_BEERS_LIST.sort(function (a, b) {
-//     return a.averageRating - b.averageRating;
-//   });
-//   categoryDisplay(shopBeersSection, SHOP_MAIN_BEERS_LIST);
-// });
 // ======================= newness =======================
 const newnessDiv = document.querySelector(".newness");
 onclickSortBy(newnessDiv, 'added');
-
-// newnessDiv.addEventListener("click", () => {
-//   SHOP_MAIN_BEERS_LIST.sort(function (a, b) {
-//     return a.added - b.added;
-//   });
-//   categoryDisplay(shopBeersSection, SHOP_MAIN_BEERS_LIST);
-// });
 // ======================= low to high price =======================
 const lowToHighPriceDiv = document.querySelector(".low-to-high-price");
 onclickSortBy(lowToHighPriceDiv, 'price');
-// lowToHighPriceDiv.addEventListener("click", () => {
-//   SHOP_MAIN_BEERS_LIST.sort(function (a, b) {
-//     return a.price - b.price;
-//   });
-//   categoryDisplay(shopBeersSection, SHOP_MAIN_BEERS_LIST);
-// });
 // ======================= high to low price =======================
 const highToLowPriceDiv = document.querySelector(".high-to-low-price");
 highToLowPriceDiv.addEventListener("click", () => {
